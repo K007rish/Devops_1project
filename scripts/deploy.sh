879e62b527c7ae0f73693722025c3e90b6d8fdc4
Krish Sutariya <krishsutariya98@gmail.com>
2026-01-08 09:27:31 +0530

    final changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+IMAGE=krsh11/react-app-prod
+
 docker stop react-app || true
 docker rm react-app || true
-docker run -d --name react-app -p 80:80 react-app:$BUILD_NUMBER
+
+docker run -d \
+  --name react-app \
+  -p 80:80 \
+  $IMAGE:latest
